96a00cd4c6d1cbfeab703bd201d7fbff2ccc0220
RNS PROJECT <93764977+renata915@users.noreply.github.com>
2021-11-12 18:32:20 +0700

    Add files via upload

---
+++ shop/allpayment.js
@@ -0,0 +1,33 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY TOKO RNS PAYMENT
+
+*さ BANK*
+1. BRI 
+	ㅁ 5074-01-003141-52-8
+		• R***A*DI
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ MINTA QRIS KE OWNER
+		• R****A
+2. OVO
+	ㅁ 0853-7297-5509
+		• R****A
+3. DANA
+	ㅁ MINTA QRIS KE OWNER
+		• R****A
+4. QRIS ALL PAYMENT
+  ㅁ MINTA QRIS KE OWNER
+   • R*****
+=> UNTUK QRIS DI KENAKAN BIAYA ADMIN SEBESAR 1.000
+*さ PULSA*
+• 0853-7297-5509
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
